d88cdec85956f1cfc3b2ef349a26270c6b91312c
maweber1991 <manuel_weber@gmx.at>
2015-04-30 16:01:02 +0200

    Commi3

--- constants.php
+++ constants.php
@@ -6,7 +6,7 @@ DEFINE('DB_SERVER', "localhost");
 //database login name
 DEFINE('DB_USER', "root");
 //database login password
-DEFINE('DB_PASS', "");
+DEFINE('DB_PASS', "mydockermillionenshow");
 
 //database name
 DEFINE('DB_DATABASE', "millionenshow");
--- millionenshow/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require_once("constants.php");
-	require_once("db.class.php");
-	require_once("smarty/libs/Smarty.class.php");
-	require_once("templatesengine.php");
-?>
